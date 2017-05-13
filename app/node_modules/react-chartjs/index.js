d86bae6f208feb25f6ba5f06ba1c670a9626878d
Themis Papameletiou <themicp@gmail.com>
2017-05-13 16:07:39 +0300

    removed node modules

--- app/node_modules/react-chartjs/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  Bar: require('./lib/bar'),
-  Doughnut: require('./lib/doughnut'),
-  Line: require('./lib/line'),
-  Pie: require('./lib/pie'),
-  PolarArea: require('./lib/polar-area'),
-  Radar: require('./lib/radar'),
-  createClass: require('./lib/core').createClass
-};
